1be500ad3d9c05ccfb4d80217dee363360dee2e6
Joshua Blatt <jtblatt@gmail.com>
2021-06-12 13:27:51 -0700

    Buddy allocator test case accidentally omitted from prior commit.

---
+++ base/tests/ESBBuddyAllocatorTest2.cpp
@@ -0,0 +1,144 @@
+#ifndef ESB_BUDDY_ALLOCATOR_H
+#include <ESBBuddyAllocator.h>
+#endif
+
+#ifndef ESB_SYSTEM_ALLOCATOR_H
+#include <ESBSystemAllocator.h>
+#endif
+
+#include <gtest/gtest.h>
+
+using namespace ESB;
+
+TEST(BuddyAllocator, Allocate) {
+  UInt32 allocSizes[] = {64, 4096, 8192, 64, 128, 1024, 2048, 256, 512};
+  void *allocations[sizeof(allocSizes) / sizeof(UInt32)];
+  memset(allocations, 0, sizeof(allocations));
+  UInt32 numAllocations = sizeof(allocSizes) / sizeof(UInt32);
+
+  BuddyAllocator allocator(16384, SystemAllocator::Instance());
+
+  for (int i = 0; i < numAllocations; ++i) {
+    ASSERT_EQ(ESB_SUCCESS, allocator.allocate(allocSizes[i] - ESB::BuddyAllocator::Overhead(), &allocations[i]));
+    ASSERT_TRUE(&allocations[i]);
+  }
+
+  void *block = NULL;
+  ASSERT_EQ(ESB_OUT_OF_MEMORY, allocator.allocate(1, &block));
+
+  for (int i = 0; i < numAllocations; ++i) {
+    ASSERT_EQ(ESB_SUCCESS, allocator.deallocate(allocations[i]));
+  }
+}
+
+TEST(BuddyAllocator, Reset) {
+  UInt32 allocSizes[] = {64, 4096, 8192, 64, 128, 1024, 2048, 256, 512};
+  void *allocations[sizeof(allocSizes) / sizeof(UInt32)];
+  memset(allocations, 0, sizeof(allocations));
+  UInt32 numAllocations = sizeof(allocSizes) / sizeof(UInt32);
+
+  BuddyAllocator allocator(16384, SystemAllocator::Instance());
+
+  for (int i = 0; i < numAllocations; ++i) {
+    ASSERT_EQ(ESB_SUCCESS, allocator.allocate(allocSizes[i] - ESB::BuddyAllocator::Overhead(), &allocations[i]));
+    ASSERT_TRUE(&allocations[i]);
+  }
+
+  void *block = NULL;
+  ASSERT_EQ(ESB_OUT_OF_MEMORY, allocator.allocate(1, &block));
+
+  ASSERT_EQ(ESB_IN_USE, allocator.reset());
+
+  for (int i = 0; i < numAllocations; ++i) {
+    ASSERT_EQ(ESB_SUCCESS, allocator.deallocate(allocations[i]));
+  }
+
+  ASSERT_EQ(ESB_SUCCESS, allocator.reset());
+}
+
+TEST(BuddyAllocator, ReallocGrow) {
+  UInt32 allocSizes[] = {64, 4096, 128, 256, 256, 128, 2048, 4096, 2048, 1024, 512, 256};
+  void *allocations[sizeof(allocSizes) / sizeof(UInt32)];
+  memset(allocations, 0, sizeof(allocations));
+  UInt32 numAllocations = sizeof(allocSizes) / sizeof(UInt32);
+
+  // Double the allocator's memory
+  BuddyAllocator allocator(16384 * 2, SystemAllocator::Instance());
+
+  // Allocate half, minus internal overhead
+  for (int i = 0; i < numAllocations; ++i) {
+    ASSERT_EQ(ESB_SUCCESS, allocator.allocate(allocSizes[i] - ESB::BuddyAllocator::Overhead(), &allocations[i]));
+    ASSERT_TRUE(&allocations[i]);
+  }
+
+  ASSERT_TRUE(allocator.reallocates());
+
+  // ~Double all allocations via realloc
+  UInt32 memoryMoved = 0;
+  for (int i = 0; i < numAllocations; ++i) {
+    void *originalAddress = allocations[i];
+    ASSERT_EQ(ESB_SUCCESS, allocator.reallocate(allocations[i], allocSizes[i] * 2 - ESB::BuddyAllocator::Overhead(),
+                                                &allocations[i]));
+    ASSERT_TRUE(&allocations[i]);
+    if (allocations[i] != originalAddress) {
+      ++memoryMoved;
+    }
+  }
+
+  ASSERT_EQ(10, memoryMoved);
+
+  // TODO must implement dry run for alloc so we can safely dealloc first
+  void *block = NULL;
+  ASSERT_EQ(ESB_OUT_OF_MEMORY, allocator.allocate(2048, &block));
+
+  for (int i = 0; i < numAllocations; ++i) {
+    ASSERT_EQ(ESB_SUCCESS, allocator.deallocate(allocations[i]));
+  }
+}
+
+// TEST(BuddyAllocator, ReallocShrink) {
+//  UInt32 allocSizes[] = {
+//      64, 4096, 8192, 64, 128, 1024, 2048, 256, 512
+//  };
+//  void *allocations[sizeof(allocSizes)/sizeof(UInt32)];
+//  memset(allocations, 0, sizeof(allocations));
+//  UInt32 numAllocations = sizeof(allocSizes)/sizeof(UInt32);
+//
+//  // Double the allocator's memory
+//  BuddyAllocator allocator(16384, SystemAllocator::Instance());
+//
+//  // Allocate half, minus internal overhead
+//  for (int i = 0; i < numAllocations; ++i) {
+//    ASSERT_EQ(ESB_SUCCESS, allocator.allocate(allocSizes[i] - ESB::BuddyAllocator::Overhead(), &allocations[i]));
+//    ASSERT_TRUE(&allocations[i]);
+//  }
+//
+//  ASSERT_TRUE(allocator.reallocates());
+//
+//  // ~Halve all allocations via realloc
+//  for (int i = 0; i < numAllocations; ++i) {
+//    ASSERT_EQ(ESB_SUCCESS, allocator.reallocate(allocations[i], allocSizes[i] / 2 - ESB::BuddyAllocator::Overhead(),
+//    &allocations[i])); ASSERT_TRUE(&allocations[i]);
+//  }
+//
+//  {
+//    void *block = NULL;
+//    ASSERT_EQ(ESB_OUT_OF_MEMORY, allocator.allocate(1, &block));
+//  }
+//
+//  // Use the other half
+//
+//  void *moreAllocations[sizeof(allocSizes)/sizeof(UInt32)];
+//  for (int i = 0; i < numAllocations; ++i) {
+//    ASSERT_EQ(ESB_SUCCESS, allocator.allocate(allocSizes[i] - ESB::BuddyAllocator::Overhead(), &moreAllocations[i]));
+//    ASSERT_TRUE(&moreAllocations[i]);
+//  }
+//
+//  void *block = NULL;
+//  ASSERT_EQ(ESB_OUT_OF_MEMORY, allocator.allocate(1, &block));
+//
+//  for (int i = 0; i < numAllocations; ++i) {
+//    ASSERT_EQ(ESB_SUCCESS, allocator.deallocate(allocations[i]));
+//    ASSERT_EQ(ESB_SUCCESS, allocator.deallocate(moreAllocations[i]));
+//  }
+//}
